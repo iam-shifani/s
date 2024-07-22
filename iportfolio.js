8acd43deff35b5e0afac43cb8272a35bdaf75bb4
Shifani <hellooo.shifani@gmail.com>
2024-07-23 01:10:43 +0530

    Add files via upload

---
+++ iportfolio.js
@@ -0,0 +1,7 @@
+/**=======================typing animation =======================*/
+var typed = new Typed(".typing", {
+    strings:[" ", "Software Engineer","Web Designer", "Web Designer", "Graphic Designer", "YouTuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
